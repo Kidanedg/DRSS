e7176469dc7193a9017f067cdacdc80bc34cd4fc
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 06:59:05 -0700

    Implement main app structure with MaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+import 'config/app_routes.dart';
+import 'config/app_theme.dart';
+
+class DRSSApp extends StatelessWidget {
+  const DRSSApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Digital Registration and Selection System',
+
+      debugShowCheckedModeBanner: false,
+
+      theme: AppTheme.lightTheme,
+
+      initialRoute: AppRoutes.splash,
+
+      routes: AppRoutes.routes,
+    );
+  }
+}
