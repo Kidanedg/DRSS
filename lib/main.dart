3d773c97fd46a7ee659760c290bdb6f79f87287a
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 21:51:36 -0700

    Update main.dart with app theme and router
    Refactor main.dart to use custom theme and router.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
+import 'package:drss/core/theme/app_theme.dart';
+import 'package:drss/firebase_options.dart';
+import 'package:drss/routes/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
-
-import 'app.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,4 +12,18 @@ Future<void> main() async {
   );
 
   runApp(const DRSSApp());
+}
+
+class DRSSApp extends StatelessWidget {
+  const DRSSApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'DRSS',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      routerConfig: appRouter,
+    );
+  }
 }
