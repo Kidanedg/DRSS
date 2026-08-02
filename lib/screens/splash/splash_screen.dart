51cdb3da25979d06e89e9392eea9de4ea5493e48
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 21:48:17 -0700

    Convert SplashScreen to StatefulWidget with timer
    Refactor SplashScreen to StatefulWidget and add a timer for navigation.

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,33 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      context.go('/login');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: const [
+
             Icon(
               Icons.how_to_reg,
               size: 100,
-              color: Colors.indigo,
+              color: Colors.blue,
             ),
+
             SizedBox(height: 20),
+
             Text(
               "DRSS",
               style: TextStyle(
-                fontSize: 36,
+                fontSize: 34,
                 fontWeight: FontWeight.bold,
               ),
             ),
+
             SizedBox(height: 10),
+
             Text(
-              "Digital Registration and Selection System",
-              textAlign: TextAlign.center,
+              "Digital Registration & Selection System",
             ),
+
+            SizedBox(height: 40),
+
+            CircularProgressIndicator(),
           ],
         ),
       ),
