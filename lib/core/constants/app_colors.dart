34d23ce917b9b9a200493b7fad4da4366bfa58bf
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 22:13:29 -0700

    Add AppColors class for color constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1E40AF);
+  static const Color secondary = Color(0xFF3B82F6);
+  static const Color accent = Color(0xFF10B981);
+
+  static const Color background = Color(0xFFF8FAFC);
+
+  static const Color textDark = Color(0xFF1E293B);
+  static const Color textLight = Color(0xFF64748B);
+
+  static const Color white = Colors.white;
+}
