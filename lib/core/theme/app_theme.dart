aab4b54c7724879ad50c6f3c8f79b145a569eca5
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 23:11:15 -0700

    Refactor light theme and UI component styles
    Updated the AppTheme class to use ColorScheme.fromSeed for light theme and adjusted various UI component themes.

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,18 +4,29 @@ import '../constants/app_colors.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: AppColors.primary,
-    scaffoldBackgroundColor: AppColors.background,
-
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppColors.primary,
-      foregroundColor: Colors.white,
-      centerTitle: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      brightness: Brightness.light,
     ),
-
+    scaffoldBackgroundColor: AppColors.background,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+    ),
+    cardTheme: CardTheme(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(double.infinity, 55),
+        minimumSize: const Size(double.infinity, 50),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
