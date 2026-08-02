6768130071b14c95a3fad603b27e8bc982fece7e
Kidanedg <gkidanedesta@gmail.com>
2026-08-01 21:47:48 -0700

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.blue,
+    scaffoldBackgroundColor: Colors.grey.shade100,
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+    ),
+  );
+}
